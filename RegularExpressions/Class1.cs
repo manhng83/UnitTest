6bcd891ba59f128f7219ae607fdf2919b5d19403
Manhng83 <manhng83@gmail.com>
2021-05-15 00:23:16 +0700

    Update code

--- RegularExpressions/Class1.cs
+++ RegularExpressions/Class1.cs
@@ -9,22 +9,29 @@ using System.Linq;
 namespace RegularExpressions
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public class Example
     {
+        public static string FileOutputName = "SearchCode.txt";
+        public static string SearchText = @"\bFunctions.Get\w*Service\b"; //DELETE ME
+        public static string Folder = @"C:\Src\"; //DELETE ME
+
         /// <summary>
         /// Search all code has text like "Functions.GetXXXService"
         /// </summary>
         public void RegexMatch_SearchCode()
         {
-            string currentDir = System.Environment.CurrentDirectory;
-            string fileName = "SearchCode.txt";
-            string filePath = System.IO.Path.Combine(currentDir, fileName);
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            string pattern = @"\bFunctions.Get\w*Service\b";
-            string folder = @"C:\Src\"; //Folder that include the (Web.config || Starup.cs) of Web Application
-            System.Collections.Generic.HashSet<string> hs = new System.Collections.Generic.HashSet<string>();
+
+            string folder = Folder; //Folder that include the (Web.config || Starup.cs) of Web Application
+            string pattern = SearchText;
+
+            string currentDir = System.Environment.CurrentDirectory;
+            string fileName = FileOutputName;
+            string filePath = System.IO.Path.Combine(currentDir, fileName);
+
+            System.Collections.Generic.HashSet<string> filesHasFound = new System.Collections.Generic.HashSet<string>();
             System.Collections.Generic.Dictionary<string, string> keyValuePairs = new System.Collections.Generic.Dictionary<string, string>();
             if (System.IO.Directory.Exists(folder))
             {
@@ -50,11 +57,14 @@ namespace RegularExpressions
                         //Only files in \Areas\...\Controllers\
                         //           OR
                         //              \Controllers\
-                        if (m.Success && !m.Value.Contains("//"))
+                        if (m.Success && !line.Contains("//") && !m.Value.Contains("//"))
                         {
                             string s = $"{f} : ({i},{m.Index})";
                             if (!keyValuePairs.Keys.Contains(f))
                             {
+                                string openToEditFile = $"devenv /edit \"{f}\"";
+                                filesHasFound.Add(openToEditFile);
+
                                 keyValuePairs.Add(f, System.IO.Path.GetFileName(f));
                                 sb.AppendLine();
                                 sb.Append(s);
@@ -67,7 +77,8 @@ namespace RegularExpressions
                         }
                     }
                 }
-                System.IO.File.WriteAllText(filePath, sb.ToString(), System.Text.Encoding.UTF8);
+                //System.IO.File.WriteAllText(filePath, sb.ToString(), System.Text.Encoding.UTF8);
+                System.IO.File.WriteAllText(filePath, string.Join(System.Environment.NewLine, filesHasFound), System.Text.Encoding.UTF8);
             }
         }
     }
